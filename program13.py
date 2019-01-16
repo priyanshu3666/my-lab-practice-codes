7b51b19cdd055c49b97ade1f4017f4106c5a5b6f
Priyanshu Shukla <44258359+priyanshu3666@users.noreply.github.com>
2019-01-16 18:46:32 +0530

    Add files via upload

---
+++ program13.py
@@ -0,0 +1,6 @@
+#program started
+p_amt = eval(input("Enter the principal amount"))
+rate = eval(input("Enter rate of interest"))
+time = eval(input("Enter time in year"))
+intrest_amt = (p_amt*rate*time)/100
+print("simple interest as per the given principal,rate and time is :",intrest_amt)
